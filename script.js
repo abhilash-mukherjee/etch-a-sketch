ab3d3d6a2fd3f7e36c87ec01123cde59289f6aaf
Abhilash Mukherjee <abhilash301267@gmail.com>
2022-08-14 12:23:46 +0530

    User can format grid number

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-const MAX_WIDTH = 960;
 const container = document.querySelector('.content');
-let gridNumber = 100;
-for(let i = 0; i< gridNumber; i++)
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const initialGridNumber = 16;
+
+function createGridHolder()
 {
-    let str = 'red';
-    let maxHeight = MAX_WIDTH/gridNumber;
-    let div = document.createElement('div');
-    div.style.border = '1px black solid';
-    div.style.backgroundColor = `${str}`;
-    div.style.flex = '1 1 auto';
-    div.style.display = 'flex';
-    div.style.flexDirection = 'row';
+    const gridHolder = document.createElement('div');
+    gridHolder.style.display = 'flex';
+    gridHolder.style.flexDirection = 'column';
+    gridHolder.style.width = '100%'
+    gridHolder.style.height = '100%'
+    return gridHolder;
+}
+
+container.appendChild(getNewGrid(initialGridNumber));
+btn.addEventListener('click',updateGrid)
+function updateGrid()
+{
+    let newGridNumber = parseInt(input.value);
+    if(isNaN(newGridNumber)) return alert(`Please eneter a number`);
+    if(newGridNumber > 100 || newGridNumber <1) 
+        return alert(`Your number should be between 0 and 100`);
+    
+    {
+        let firstChild = container.firstElementChild;
+        container.removeChild(firstChild);
+        container.appendChild(getNewGrid(newGridNumber));
+    }
+}
+function getNewGrid(gridNumber)
+{
+    let gridHolder = createGridHolder();
     for(let i = 0; i< gridNumber; i++)
     {
-        let childDiv = document.createElement('div');
-        childDiv.style.border = '1px black solid';
-        childDiv.style.backgroundColor = `yellow`;
-        childDiv.style.flex = '1 1 auto';
-        div.appendChild(childDiv);
+        let div = document.createElement('div');
+        div.style.border = '1px black solid';
+        div.style.backgroundColor = `red`;
+        div.style.flex = '1 1 auto';
+        div.style.display = 'flex';
+        div.style.flexDirection = 'row';
+        div.style.boxSizing = 'border-box';
+        for(let i = 0; i< gridNumber; i++)
+        {
+            let childDiv = document.createElement('div');
+            childDiv.style.boxSizing = 'border-box';
+            childDiv.style.border = '1px black solid';
+            childDiv.style.backgroundColor = `yellow`;
+            childDiv.style.flex = '1 1 auto';
+            div.appendChild(childDiv);
+        }
+        gridHolder.appendChild(div);
     }
-    container.appendChild(div);
+    return gridHolder;
 }
